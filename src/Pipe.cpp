86479470ecfbb7c5786950a485dc59606575a459
Fabio Kreusch <fabiokr@gmail.com>
2014-07-19 17:01:42 -0300

    Adds pipe sprite positions

--- src/Pipe.cpp
+++ src/Pipe.cpp
@@ -1,19 +1,68 @@
 #include "Pipe.h"
 
-Pipe::Pipe(SDL_Surface *sprite_param, bool left_param, bool right_param, bool top_param, bool down_param)
+Pipe::Pipe(SDL_Surface *sprite_param, SDL_Surface *alt_sprite_param, bool top_param, bool right_param, bool down_param, bool left_param)
 {
     sprite = sprite_param;
+    alt_sprite = alt_sprite_param;
 
-    left = left_param;
-    right = right_param;
+
     top = top_param;
+    right = right_param;
     down = down_param;
+    left = left_param;
 
-    // TODO - Use params to set sprite_position
-    sprite_position.w = sprite_position.h = 48;
-    sprite_position.x = sprite_position.y = 0;
+    sprite_position.w = WIDTH;
+    sprite_position.h = HEIGHT;
+
+    // Sets up the sprites position based on the pipe openings
+    if(top == true && right == true && down == true && left == true) {
+        sprite_position.x = 0;
+        sprite_position.y = 0;
+    } else if(top == true && right == true && down == true) {
+        sprite_position.x = WIDTH * 4;
+        sprite_position.y = 0;
+    } else if(top == true && right == true && left == true) {
+        sprite_position.x = WIDTH * 3;
+        sprite_position.y = 0;
+    } else if(top == true && down == true && left == true) {
+        sprite_position.x = WIDTH * 2;
+        sprite_position.y = 0;
+    } else if(right == true && down == true && left == true) {
+        sprite_position.x = WIDTH;
+        sprite_position.y = 0;
+    } else if(top == true && right == true) {
+        sprite_position.x = WIDTH;
+        sprite_position.y = HEIGHT;
+    } else if(top == true && down == true) {
+        sprite_position.x = WIDTH * 3;
+        sprite_position.y = HEIGHT;
+    } else if(top == true && left == true) {
+        sprite_position.x = WIDTH * 2;
+        sprite_position.y = HEIGHT;
+    } else if(right == true && down == true) {
+        sprite_position.x = WIDTH * 5;
+        sprite_position.y = 0;
+    } else if(right == true && left == true) {
+        sprite_position.x = WIDTH * 4;
+        sprite_position.y = HEIGHT;
+    } else if(down == true && left == true) {
+        sprite_position.x = 0;
+        sprite_position.y = HEIGHT;
+    } else if(top == true) {
+        sprite_position.x = WIDTH * 2;
+        sprite_position.y = HEIGHT * 2;
+    } else if(right == true) {
+        sprite_position.x = WIDTH * 5;
+        sprite_position.y = HEIGHT;
+    } else if(down == true) {
+        sprite_position.x = WIDTH;
+        sprite_position.y = HEIGHT * 2;
+    } else if(left == true) {
+        sprite_position.x = 0;
+        sprite_position.y = HEIGHT * 2;
+    }
 }
 
-void Pipe::Draw (SDL_Surface* surface, SDL_Rect *position) {
+void Pipe::Draw(SDL_Surface* surface, SDL_Rect* position) {
     SDL_BlitSurface(sprite, &sprite_position, surface, position);
 }
